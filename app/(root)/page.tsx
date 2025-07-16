d6de336631ce4f6c38c361fe5fb36af20e37f990
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-07-16 20:07:34 +0200

    updated page.tsx, VideoCard.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,7 +3,7 @@ import VideoCard from '@/components/VideoCard'
 import React from 'react'
 
 const Page = () => {
-  const createdAt = new Date("2025-05-01T06:25:54.437");
+  const createdAt = new Date("2025-05-01");
 
   return (
     <main className="wrapper page">
@@ -11,7 +11,7 @@ const Page = () => {
       <h1 className="text-2xl font-karla text-shadow-lg">Welcome to MemoCast</h1>
       <VideoCard
         id="1"
-        title="SnapChat Message - 30 June 2025"
+        title="SnapChat Message"
         thumbnail="/assets/samples/thumbnail(1).png"
         createdAt={createdAt}
         userImg="/assets/images/test.png"
--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -24,35 +24,55 @@ const VideoCard = ({
 
     return (
         <Link
-        href={`/video/${id}`}
-        className="relative block max-w-90 rounded-2xl shadow-md overflow-hidden bg-white"
+            href={`/video/${id}`}
+            className="relative block max-w-90 rounded-2xl shadow-md overflow-hidden bg-white"
         >
-            <div className="relative w-auto h-[210px] rounded-sm overflow-hidden">
+            <div
+                className="relative w-auto h-[210px] rounded-sm overflow-hidden"
+            >
                 <Image
-                src={thumbnail}
-                alt="thumbnail"
-                fill
-                sizes="(max-width: 768px) 100vw, 290px"
-                className="object-cover rounded-xl"
-                priority={true}
+                    src={thumbnail}
+                    alt="thumbnail"
+                    fill
+                    sizes="(max-width: 768px) 100vw, 290px"
+                    className="object-cover rounded-xl"
+                    priority={true}
                 />
             </div>
 
-            <article className="flex flex-col gap-3 px-3.5 pt-4 pb-4.5 rounded-b-2xl">
-                <div className="flex gap-2 justify-between">
-                    <figure className="flex items-center gap-1.5">
+            <article
+                className="flex flex-col gap-3 px-3.5 pt-4 pb-4.5 rounded-b-2xl"
+            >
+                <div
+                    className="flex gap-2 justify-between"
+                >
+                    <figure
+                        className="flex items-center gap-1.5"
+                    >
                         <Image
                             src={userImg}
                             alt="avatar"
                             width={34}
                             height={34}
                         />
-                        <figcaption className="flex flex-col gap-0.5">
-                            <h3 className="text-xs font-semibold text-[#1d073a]">{ username }</h3>
-                            <p className="text-xs text-gray-100 font-normal capitalize">{ visibility }</p>
+                        <figcaption
+                            className="flex flex-col gap-0.5"
+                        >
+                            <h3
+                                className="text-xs font-semibold text-[#1d073a]"
+                            >
+                                { username }
+                            </h3>
+                            <p
+                                className="text-xs text-gray-100 font-normal capitalize"
+                            >
+                                { visibility }
+                            </p>
                         </figcaption>
                     </figure>
-                    <aside className="flex items-center gap-1">
+                    <aside
+                        className="flex items-center gap-1"
+                    >
                         <Image
                             src="/assets/icons/eye.svg"
                             alt="views"
@@ -62,9 +82,14 @@ const VideoCard = ({
                                 filter: 'brightness(0) saturate(100%) invert(16%) sepia(51%) saturate(2261%)    hue-rotate(229deg) brightness(92%) contrast(101%)'
                             }}
                         />
-                        <span className="text-xs text-gray-100 font-normal">{ views }</span>
+                        <span
+                            className="text-xs text-gray-100 font-normal"
+                        >
+                            { views }
+                        </span>
                     </aside>
                 </div>
+                <h2 className="text-base text-dark-100 font-semibold truncate">{ title } - {" "} { formattedDate }</h2>
             </article>
         </Link>
     );
