7e3d2192d7c6eaba280d91278887d9b0ec7d9fc6
Guillaume Mehats <guillaume.mehats@gmail.com>
2025-07-16 10:23:41 +0200

    removed .history folder

--- .history/app/(root)/layout_20250716101514.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react"
-
-const Layout = ({ children } : { children: ReactNode}) => {
-    return (
-        <div>Layout</div>
-    )
-}
-
-export default Layout 
--- .history/components/Navbar_20250716101840.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Navbar = () => {
-    return (
-        <div className='text-xl font-karla p-8 m-0 flex justify-center'>Navbar</div>
-    )
-}
-
-export default Navbar
